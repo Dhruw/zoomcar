3355448ff1444885d29cd6ca98fbf9e874a14e59
dhruw <divyanshudhruw@gmail.com>
2015-11-21 19:32:27 +0530

    Ascending Descending Buttons

--- assets/app.js
+++ assets/app.js
@@ -8,6 +8,8 @@
             $http.get('https://zoomcar-ui.0x10.info/api/courier?type=json&query=list_parcel').success(function (response) {
                 $scope.parcels = response.parcels;
                 $scope.currentItem = response.parcels[0];
+                $scope.list_order='name';
+                $scope.current_order='Ascending';
                 //$scope.setMap(response.parcels[0].live_location)
                 console.log(response.parcels[0].live_location);
                 angular.element(".btn-group-vertical>.btn:first-child").trigger('click');
@@ -26,111 +28,142 @@
             $scope.setItem = function (x) {
                 $scope.currentItem = x;
             }
-            
-            
-            
-            
-            
-            
-            
-            
-            
-           
-        $scope.setMap = function (item_position) {
-            var lat = item_position.latitude,
-                lng = item_position.longitude,
-                latlng = new google.maps.LatLng(lat, lng),
-                image = 'http://www.google.com/intl/en_us/mapfiles/ms/micons/blue-dot.png';
 
-            var mapOptions = {
-                center: new google.maps.LatLng(lat, lng),
-                zoom: 13,
-                mapTypeId: google.maps.MapTypeId.ROADMAP,
-                panControl: true,
-                panControlOptions: {
-                    position: google.maps.ControlPosition.TOP_RIGHT
-                },
-                zoomControl: true,
-                zoomControlOptions: {
-                    style: google.maps.ZoomControlStyle.LARGE,
-                    position: google.maps.ControlPosition.TOP_left
+            $scope.reorder_list = function(x)
+            {
+                switch(x)
+                {
+                    case "name":
+                        $scope.list_order = '-name';
+                        $scope.current_order='Descending';
+                        break;
+                    case "-name":
+                        $scope.list_order = 'name';
+                        $scope.current_order='Ascending';
+                        break;
+                    case "price":
+                        $scope.list_order = '-price';
+                        $scope.current_order='Descending';
+                        break;
+                    case "-price":
+                        $scope.list_order = 'price';
+                        $scope.current_order='Ascending';
+                        break;
+                    case "weight":
+                        $scope.list_order = '-weight';
+                        $scope.current_order='Descending';
+                        break;
+                    case "-weight":
+                        $scope.list_order = 'weight';
+                        $scope.current_order='Ascending';
+                        break;
                 }
-            },
-                map = new google.maps.Map(document.getElementById('map_canvas'), mapOptions),
-                marker = new google.maps.Marker({
-                    position: latlng,
-                    map: map,
-                    icon: image
-                });
-
-            var input = document.getElementById('searchTextField');
-            var autocomplete = new google.maps.places.Autocomplete(input, {
-                types: ["geocode"]
-            });
-
-            autocomplete.bindTo('bounds', map);
-            var infowindow = new google.maps.InfoWindow();
-
-            google.maps.event.addListener(autocomplete, 'place_changed', function (event) {
-                infowindow.close();
-                var place = autocomplete.getPlace();
-                if (place.geometry.viewport) {
-                    map.fitBounds(place.geometry.viewport);
-                } else {
-                    map.setCenter(place.geometry.location);
-                    map.setZoom(17);
-                }
-
-                moveMarker(place.name, place.geometry.location);
-                $('.MapLat').val(place.geometry.location.lat());
-                $('.MapLon').val(place.geometry.location.lng());
-            });
-            google.maps.event.addListener(map, 'click', function (event) {
-                $('.MapLat').val(event.latLng.lat());
-                $('.MapLon').val(event.latLng.lng());
-                infowindow.close();
-                var geocoder = new google.maps.Geocoder();
-                geocoder.geocode({
-                    "latLng": event.latLng
-                }, function (results, status) {
-                    console.log(results, status);
-                    if (status == google.maps.GeocoderStatus.OK) {
-                        console.log(results);
-                        var lat = results[0].geometry.location.lat(),
-                            lng = results[0].geometry.location.lng(),
-                            placeName = results[0].address_components[0].long_name,
-                            latlng = new google.maps.LatLng(lat, lng);
-
-                        moveMarker(placeName, latlng);
-                        $("#searchTextField").val(results[0].formatted_address);
-                    }
-                });
-            });
-
-            function moveMarker(placeName, latlng) {
-                marker.setIcon(image);
-                marker.setPosition(latlng);
-                infowindow.setContent(placeName);
-                //infowindow.open(map, marker);
             }
-        }
-    
 
-            
-            
-            
-            
-            
-            
-            
-            
-            
-            
-            
-            
-            
-            
-            
+
+
+
+
+
+
+
+
+
+            $scope.setMap = function (item_position) {
+                var lat = item_position.latitude,
+                    lng = item_position.longitude,
+                    latlng = new google.maps.LatLng(lat, lng),
+                    image = 'http://www.google.com/intl/en_us/mapfiles/ms/micons/blue-dot.png';
+
+                var mapOptions = {
+                    center: new google.maps.LatLng(lat, lng),
+                    zoom: 13,
+                    mapTypeId: google.maps.MapTypeId.ROADMAP,
+                    panControl: true,
+                    panControlOptions: {
+                        position: google.maps.ControlPosition.TOP_RIGHT
+                    },
+                    zoomControl: true,
+                    zoomControlOptions: {
+                        style: google.maps.ZoomControlStyle.LARGE,
+                        position: google.maps.ControlPosition.TOP_left
+                    }
+                },
+                    map = new google.maps.Map(document.getElementById('map_canvas'), mapOptions),
+                    marker = new google.maps.Marker({
+                        position: latlng,
+                        map: map,
+                        icon: image
+                    });
+
+                var input = document.getElementById('searchTextField');
+                var autocomplete = new google.maps.places.Autocomplete(input, {
+                    types: ["geocode"]
+                });
+
+                autocomplete.bindTo('bounds', map);
+                var infowindow = new google.maps.InfoWindow();
+
+                google.maps.event.addListener(autocomplete, 'place_changed', function (event) {
+                    infowindow.close();
+                    var place = autocomplete.getPlace();
+                    if (place.geometry.viewport) {
+                        map.fitBounds(place.geometry.viewport);
+                    } else {
+                        map.setCenter(place.geometry.location);
+                        map.setZoom(17);
+                    }
+
+                    moveMarker(place.name, place.geometry.location);
+                    $('.MapLat').val(place.geometry.location.lat());
+                    $('.MapLon').val(place.geometry.location.lng());
+                });
+                google.maps.event.addListener(map, 'click', function (event) {
+                    $('.MapLat').val(event.latLng.lat());
+                    $('.MapLon').val(event.latLng.lng());
+                    infowindow.close();
+                    var geocoder = new google.maps.Geocoder();
+                    geocoder.geocode({
+                        "latLng": event.latLng
+                    }, function (results, status) {
+                        console.log(results, status);
+                        if (status == google.maps.GeocoderStatus.OK) {
+                            console.log(results);
+                            var lat = results[0].geometry.location.lat(),
+                                lng = results[0].geometry.location.lng(),
+                                placeName = results[0].address_components[0].long_name,
+                                latlng = new google.maps.LatLng(lat, lng);
+
+                            moveMarker(placeName, latlng);
+                            $("#searchTextField").val(results[0].formatted_address);
+                        }
+                    });
+                });
+
+                function moveMarker(placeName, latlng) {
+                    marker.setIcon(image);
+                    marker.setPosition(latlng);
+                    infowindow.setContent(placeName);
+                    //infowindow.open(map, marker);
+                }
+            }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
         });
 
 
